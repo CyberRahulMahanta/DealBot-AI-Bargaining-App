332c980763eab11fb0ec4b878b5d660bc4b80083
Rahul mahanta <mahantorahul503@gmail.com>
2026-03-02 13:53:14 +0530

    Updated login UI, resized icons, improved social buttons

--- app/src/main/java/com/example/deal/MainActivity.kt
+++ app/src/main/java/com/example/deal/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.example.deal
 
 import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
